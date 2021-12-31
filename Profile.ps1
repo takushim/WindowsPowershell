6b4b67d16eb004df298f683f09201d1ddf2c2b9d
Takushi Miyoshi <tmiyoshi@nih.gov>
2021-12-31 10:19:37 -0500

    The first commit

---
+++ Profile.ps1
@@ -0,0 +1,98 @@
+Set-Alias grep Select-String
+Set-Alias which Get-Command
+Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+(Get-PSReadLineOption).HistorySearchCursorMovesToEnd = $True
+
+#$host.PrivateData.ErrorForegroundColor = 'Black'
+#$host.PrivateData.ErrorBackgroundColor = 'White'
+Set-PSReadLineOption -Colors @{
+  Command            = 'White'
+  Number             = 'DarkGreen'
+  Member             = 'White'
+  Operator           = 'DarkGreen'
+  Type               = 'White'
+  Variable           = 'DarkGreen'
+  Parameter          = 'DarkGreen'
+  ContinuationPrompt = 'White'
+  Default            = 'White'
+}
+
+function prompt {
+    $idx = $pwd.ProviderPath.LastIndexof("\") + 1
+    $cdn = $pwd.ProviderPath.Remove(0, $idx)
+
+    (Get-Host).UI.RawUI.WindowTitle = $cdn + " (" + $pwd + ") "
+
+   if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+         [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        Write-Host $env:UserDomain -NoNewLine -ForegroundColor "Red"
+        Write-Host ":" -NoNewLine -ForegroundColor "White"
+        Write-Host $cdn -NoNewLine -ForegroundColor "Red"
+        return "# "
+    }
+
+    Write-Host $env:UserDomain -NoNewLine -ForegroundColor "Green"
+    Write-Host ":" -NoNewLine -ForegroundColor "White"
+    Write-Host $cdn -NoNewLine -ForegroundColor "Green"
+    return "$ "
+}
+
+function goenv ($name = "gpu") {
+    $envfile = [IO.Path]::Combine($HOME, ".venv", $name, "Scripts\Activate.ps1")
+    if (Test-Path($envfile)) {
+        . $envfile
+    }
+    else {
+        Write-Host "No environment named:" $name 
+    }
+}
+
+function lsenv {
+    $envfolder = [IO.Path]::Combine($HOME, ".venv")
+    $envlist = (Get-ChildItem -Directory -Name $envfolder)
+    Write-Host "Folder:" $envfolder
+    Write-Host "Environments:" $envlist
+}
+
+function cdenv {
+    $envfolder = [IO.Path]::Combine($HOME, ".venv")
+    Set-Location $envfolder
+}
+
+function goto ($name = "home") {
+    $spim = "Y:\spim\"
+    $lsm = "Y:\lsm"
+    $tirf = "Y:\tirf"
+    switch ($name) {
+    "bin"  {$folder = [IO.Path]::Combine($HOME, $name)}
+    "env"  {$folder = [IO.Path]::Combine($HOME, $name)}
+    "home" {$folder = "$HOME"}
+    "2021" {$folder = [IO.Path]::Combine($spim, $name)}
+    "2020" {$folder = [IO.Path]::Combine($spim, $name)}
+    "2019" {$folder = [IO.Path]::Combine($spim, $name)}
+    "lsm"  {$folder = $lsm}
+    "tirf" {$folder = $tirf}
+    "temp" {$folder = [IO.Path]::Combine($HOME, "Desktop", "temp")}
+    default {
+        Write-Host "Not matched: {0}" -f $name
+        $folder = "."
+    }
+    }
+    Set-Location $folder
+}
+
+function keepconnect {
+    while (1) {
+        Write-Output "keepalive" > temporary_file_to_keep_connection.txt
+        Write-Output "keeping access"
+        Start-Sleep 5
+    }
+}
+
+function pshell {
+    start-process powershell
+}
+
